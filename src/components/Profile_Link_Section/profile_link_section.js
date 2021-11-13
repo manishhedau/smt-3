493c859ea47b46477665d1227443816bb0d83e8d
manishhedau <hedaumanish@gmail.com>
2021-11-13 21:17:32 +0530

    third commit

--- src/components/Profile_Link_Section/profile_link_section.js
+++ src/components/Profile_Link_Section/profile_link_section.js
@@ -1,5 +1,6 @@
 import './profile_link_section.css';
 import { Tooltip } from '@mui/material';
+import { Link } from "react-router-dom";
 import { useState } from 'react';
 
 const ProfileLinkSection = (props) => {
@@ -19,11 +20,11 @@ const ProfileLinkSection = (props) => {
                 <i className="far fa-eye"></i>
 
                 <div id="profile-link">
-                    <Tooltip title={<h6 style={{width: "100%"}}>{text}</h6>}>
+                    <Tooltip title={<h6 style={{ width: "100%" }}>{text}</h6>}>
                         <h5 onClick={handleClick}>https://skyhype.in/:userId</h5>
                     </Tooltip>
                 </div>
-
+                <Link to="/user/61815b950cda90d1d126f33b">user Profile</Link>
                 <i className="fa-solid fa-share-nodes"></i>
                 <i className="fas fa-qrcode"></i>
             </div>
